91cf3928681b5254dfebcd9f08154c3aa0bd6e6d
Victor Rincon <vrincon@oriontek.do>
2020-04-27 01:08:07 -0400

    Update fixer config

--- application/config/development/fixer.php
+++ application/config/development/fixer.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['fixer_api_key']        = 'dadbb323965b97d9d40d64e01245d293';
+$config['fixer_api_key']        = 'YOU API KEY';
 $config['fixer_base_rate']      = 'EUR';
